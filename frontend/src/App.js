9cd7ed7fb632778e2c458f2360a719d796480a61
Suds Kumar <suds123@gmail.com>
2025-08-31 18:55:20 +0100

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// frontend/src/App.js
+import React from 'react';
+import ChurnDashboard from './components/ChurnDashboard';
+import './App.css';
+
+function App() {
+  return (
+    
+      
+    
+  );
+}
+
+export default App;
